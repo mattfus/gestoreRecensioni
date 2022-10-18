830f6b1569c8c1d9fdfb6c19817e7034a57dbbe8
Matteo <matteofusaro1@gmail.com>
2022-10-18 16:07:59 +0200

    First Commit

---
+++ src/main/java/com/example/gestorerecensioni/GestoreRecensioniApplication.java
@@ -0,0 +1,13 @@
+package com.example.gestorerecensioni;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestoreRecensioniApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestoreRecensioniApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gestorerecensioni/GestoreRecensioniApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gestorerecensioni;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestoreRecensioniApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
